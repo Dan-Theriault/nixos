51be686fbcbd390a520eeb4f3fd086fcc3ef4197
Dan Theriault <dannymt97@gmail.com>
2017-07-27 19:36:23 -0400

    hide cursor on lock

--- scripts/rofi-sys.sh
+++ scripts/rofi-sys.sh
@@ -3,8 +3,8 @@
   action=$( echo 'Suspend,Lock,Hibernate,Logout,Restart,Shutdown' | rofi -dmenu -sep ',' -p 'System:' -i -no-custom)
 } && {
   case $action in 
-      Suspend) systemctl suspend && lock -f Source-Sans-Pro-Light -- maim && sleep 2 && ~/Scripts/resume.sh;;
-      Lock) lock -f Iosevka -- maim && ~/Scripts/resume.sh;;
+      Suspend) systemctl suspend && i3lock-fancy -f Overpass-Black -t "TYPE TO UNLOCK" -- maim -u && sleep 2 && ~/Scripts/resume.sh;;
+      Lock) i3lock-fancy -f Overpass-Black -t "TYPE TO UNLOCK" -- maim -u && ~/Scripts/resume.sh;;
       Hibernate) systemctl hibernate;;
       Restart) systemctl reboot;;
       Logout) i3-msg exit;;
