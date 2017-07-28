4c99d14c5eb568a6d43747a631ccf6074992356e
Dan Theriault <dannymt97@gmail.com>
2017-07-27 20:31:07 -0400

    Stored fish prompt on-machine

--- base/fish.nix
+++ base/fish.nix
@@ -3,9 +3,8 @@
 {
   programs.fish = {
     enable = true;
-    promptInit = builtins.readFile ( builtins.fetchurl { 
-      url = "https://raw.githubusercontent.com/oh-my-fish/theme-robbyrussell/master/fish_prompt.fish";
-    } );
+    promptInit = builtins.readFile ./fish_prompt.fish;   
+    
     shellAliases = {
       vim = "vim --servername vim";
     };
