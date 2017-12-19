abb2666d4848090776d76ae8b9d50e297110c811
Daniel Theriault <dannymt97@gmail.com>
2017-12-18 22:04:43 -0500

    Added main user to scanner, lpadmin, and bluetooth groups

--- base/users.nix
+++ base/users.nix
@@ -7,7 +7,7 @@
     defaultUserShell = pkgs.bash;
     users.dtheriault3 = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" ];
+      extraGroups = [ "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" "scanner" "lpadmin" "bluetooth" ];
       initialPassword = "hagan lio"; # change immediately after install with passwd
     };
   };
