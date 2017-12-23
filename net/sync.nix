59a370bd713cfdb5629cd0638638ef80ea16bcda
Daniel Theriault <dannymt97@gmail.com>
2017-12-23 00:13:33 -0500

    Gave in on getting unison to function. Re-installed syncthing.

--- net/sync.nix
+++ net/sync.nix
@@ -7,5 +7,13 @@
   # - Remotes configuration
   # - Icron script
   # - profiles
-  environment.systemPackages = with pkgs; [ ocamlPackages.unison ];
+  environment.systemPackages = with pkgs; [ 
+    # Probably dropping unison. It's utterly convinced it has synced files... that it hasn't.
+    ocamlPackages.unison # unix-style tool wrapping ssh and rsync.
+
+    # As much as I don't want to use syncthing (due to the global discovery and non-declarative nature),
+    # it seems to be the best option right now.
+    # So here's another TODO: stop using syncthing
+    syncthing 
+  ];
 }
