41ab63db739a4100cc84be0c0cb117ee6a8f2d86
Daniel Theriault <dannymt97@gmail.com>
2017-12-22 21:24:05 -0500

    Added IBM Plex, and set it as the default

--- misc/fonts.nix
+++ misc/fonts.nix
@@ -1,16 +1,41 @@
 { config, pkgs, ... }:
 
+let
+  ibm-plex = pkgs.fetchzip rec {
+    name = "ibm-plex-0.5.3";
+
+    url = "https://github.com/IBM/type/archive/v0.5.3.zip";
+
+    postFetch = ''
+      mkdir -p $out/share/{doc,fonts}
+      unzip -j $downloadedFile \*.otf      -d $out/share/fonts/opentype
+      unzip -j $downloadedFile \*README.md -d "$out/share/doc/${name}"
+    '';
+
+    sha256 = "1ghj2lqc5m33qa55h8ag6xwdqbwc9gdxncf66jr5fd5fadjhrfr8";
+
+    meta = with pkgs.stdenv.lib; {
+      description = "IBM's new corporate typeface. Mono, Sans, and Serif with eight weights.";
+      homepage = https://ibm.github.io/type/;
+      license = licenses.ofl;
+      platforms = platforms.all;
+      # maintainers = with maintainers; [ dan-theriault ];
+    };
+  };
+
+in
+
 {
   fonts = {
     enableFontDir = true;
     enableDefaultFonts = true;
     fontconfig.penultimate.enable = true;
     fontconfig.defaultFonts = {
-      monospace = [ "Source Code Pro" ];
-      sansSerif = [ "Source Sans Pro" ];
-      serif =     [ "Source Serif Pro" ];
+      monospace = [ "IBM Plex Mono" ];
+      sansSerif = [ "IBM Plex Sans" ];
+      serif =     [ "IBM Plex Serif" ];
     };
-    fonts = with pkgs; [
+    fonts = [ ibm-plex ] ++ ( with pkgs; [
       google-fonts # ~300mb tarball download. Slow. Lots of junk. But oh-so-many fonts!
 
       dejavu_fonts
@@ -29,6 +54,6 @@
       source-code-pro
       source-sans-pro
       source-serif-pro
-    ];
+    ] );
   };
 }
