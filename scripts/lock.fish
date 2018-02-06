029b9112c5e0c1d94648759d28ad278046118420
Daniel Theriault <dannymt97@gmail.com>
2018-02-05 21:34:53 -0500

    Now lock computer and pause music on CTRL-x

--- scripts/lock.fish
+++ scripts/lock.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
-/run/current-system/sw/bin/i3lock-fancy -f Overpass-Black -t "TYPE TO UNLOCK" -- maim
+/run/current-system/sw/bin/playerctl pause
+/run/current-system/sw/bin/i3lock-fancy -f Overpass-Black -t "TYPE TO UNLOCK" -- maim; 
--- scripts/rofi-sys.sh
+++ scripts/rofi-sys.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-{
-  action=$( echo 'Suspend,Lock,Hibernate,Logout,Restart,Shutdown' | rofi -dmenu -sep ',' -p 'System:' -i -no-custom)
-} && {
-  case $action in 
-      Suspend) systemctl suspend;;
-      Lock) loginctl lock-session;;
-      Hibernate) systemctl hibernate;;
-      Restart) systemctl reboot;;
-      Logout) i3-msg exit;;
-      Shutdown) systemctl poweroff;;
-  esac
-}
+# {
+#   action=$( echo 'Suspend,Lock,Hibernate,Logout,Restart,Shutdown' | rofi -dmenu -sep ',' -p 'System:' -i -no-custom)
+# } && {
+#   case $action in 
+#       Suspend) systemctl suspend;;
+#       Lock) loginctl lock-session;;
+#       Hibernate) systemctl hibernate;;
+#       Restart) systemctl reboot;;
+#       Logout) i3-msg exit;;
+#       Shutdown) systemctl poweroff;;
+#   esac
+# }
+loginctl lock-session
