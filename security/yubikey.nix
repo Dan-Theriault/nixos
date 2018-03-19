c46b6efb7b3977c3f0c2e773629551db5ec5fc11
Daniel Theriault <git.dan@theriault.codes>
2018-03-19 13:53:16 -0400

    enabled pcscd service, required for yubikey u2f mode

--- security/yubikey.nix
+++ security/yubikey.nix
@@ -1,11 +1,16 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     yubikey-personalization
+    yubikey-manager
+
+    ccid
+    pcsclite pcsctools
   ];
 
-  services.udev.packages = with pkgs; [
-    yubikey-personalization
-  ];
+  services = {
+    udev.packages = with pkgs; [ yubikey-personalization ];
+    pcscd.enable = true;
+  };
 }
