64b77ae1b0a39e9559ec2ec63bc6da65f440f784
Daniel Theriault <git.dan@theriault.codes>
2018-04-24 15:01:00 -0400

    Installed kdenlive

--- desktop/pkgs.nix
+++ desktop/pkgs.nix
@@ -1,42 +1,24 @@
 { config, pkgs, ... }:
 
 let
-  keepassxc = pkgs.keepassx-community.overrideDerivation (attrs: rec {
-    version = "2.3.0";
-    buildInputs = attrs.buildInputs ++ [ 
-      pkgs.libargon2 
-      pkgs.curlFull.dev 
-      pkgs.libsodium.dev
-    ];
-    patches = [];
-    cmakeFlags = attrs.cmakeFlags ++ [ 
-      "-DWITH_XC_NETWORKING=ON"
-      "-DWITH_XC_BROWSER=ON"
-      "-DWITH_XC_SSHAGENT=ON"
-      "-DKEEPASSXC_BUILD_TYPE=Release"
-      "-DWITH_XC_HTTP=OFF"
-    ];
-    src = pkgs.fetchFromGitHub {
-      owner = "keepassxreboot";
-      repo = "keepassxc";
-      rev = "2.3.0";
-      sha256 = "1zch1qbqgphhp2p2kvjlah8s337162m69yf4y00kcnfb3539ii5f";
-    };
-  });
-
   mozilla = builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz;
   firefox-custom = pkgs.buildEnv {
     name = "firefox-custom";
     paths = [ 
       # pkgs.latest.firefox-beta-bin
       pkgs.firefox-beta-bin
-      # pkgs.keepassx-community 
       pkgs.ffmpeg pkgs.libav
-      keepassxc
+      pkgs.keepassxc
     ];
   };
 in
 {
+  nixpkgs.config.overridePackges = {
+    kdenlive = pkgs.kdenlive.override {
+      buildInputs = [ pkgs.frei0r ];
+    };
+  };
+
   environment.systemPackages =  with pkgs; [
     antimony gmsh # weird CAD and a STL viewer.
     arandr
@@ -44,11 +26,10 @@ in
     libav
     libqalculate
     libreoffice-fresh detox
+    kdenlive
     mpv youtube-dl
     meld # graphical diffs
     sc-im
-    steam
-    steam-run
     wireshark
     xst
     zathura
