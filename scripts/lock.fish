4d45ac00af1da90ed1cee1b0e2676d885be706ce
Daniel Theriault <git.dan@theriault.codes>
2018-04-24 14:56:22 -0400

    restart vpn in lock script

--- scripts/lock.fish
+++ scripts/lock.fish
@@ -2,3 +2,4 @@
 
 /run/current-system/sw/bin/playerctl pause
 /run/current-system/sw/bin/i3lock-fancy -f Overpass-Black -t "TYPE TO UNLOCK" -- maim; 
+/run/current-system/sw/bin/systemctl restart openvpn-mullvad-main
