d2d6676ae483d25b8f244d933a72dac7b6de43d1
Daniel Theriault <dannymt97@gmail.com>
2020-10-24 21:23:07 -0400

    setup spotifyd

--- desktop/audio.nix
+++ desktop/audio.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 
+let
+  spotifyd = pkgs.spotifyd.override { withMpris = true; withKeyring = true; dbus = pkgs.dbus; };
+  spotifydConf = pkgs.writeText "spotifyd.conf" ''
+    [global]
+    username = dan@theriault.codes
+    use_keyring = true
+    device_name = spotifyd 
+    device_type = computer
+    backend = pulseaudio
+    bitrate = 320
+  '';
+in
 {
   hardware.pulseaudio = {
     enable = true;
@@ -23,13 +35,23 @@
     sonata
     clerk
 
-    spotify
+    spotify-tui
   ] );
-  # TODO: spotifyd, spotify-tui, remove spotify
 
   boot.kernelModules = [ "snd_usb_audio" ]; # external DAC/AMP support
 
   programs.fish.shellAliases = {
     lyrics = "glyrc lyrics -a (playerctl metadata artist 2>/dev/null) -t (playerctl metadata title 2>/dev/null) -v 0 -w /tmp/lyrics; and cat /tmp/lyrics";
+  };
+
+  systemd.user.services.spotifyd = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network-online.target" "sound.target" ];
+    description = "spotifyd, a Spotify playing daemon";
+    serviceConfig = {
+      ExecStart = "${spotifyd}/bin/spotifyd --no-daemon --config-path ${spotifydConf}";
+      Restart = "always";
+      RestartSec = 12;
+    };
   };
 }
